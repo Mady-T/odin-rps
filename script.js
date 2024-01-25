1831c86d129fffe601ad741191519e9a2c27a530
Mady-T <adithya5463sajith@gmail.com>
2024-01-26 03:17:07 +0530

    Create stringifyRPS, add console output of choices

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-function playRound(playerScore, cpuScore, roundNumber) {
-    let outcome = evalRound(
-        () => prompt("Rock Paper Scissors!").toLowerCase(),
-        () => Math.floor(Math.random() * 3)
-    );
+let playerScore = 0;
+let cpuScore = 0;
+let roundCount = 1;
+
+function playRound(playerScore, cpuScore, roundCount) {
+    console.group();
+    console.log(`Round ${roundCount}:
+                Player: ${playerScore}
+                CPU: ${cpuScore}
+                `);
+    
+    let playerChoice = prompt("Rock Paper Scissors!").toLowerCase();
+    console.log(`You chose ${playerChoice}`);
+    let cpuPick = Math.floor(Math.random() * 3);
+    console.log('CPU chose ${stringifyRPS(cpuPick)}');
+    let outcome = evalRound(enumerateRPS(playerChoice), cpuPick);
     if (outcome === 'win') {
         playerScore++;
     } else if (outcome === 'loss') {
         cpuScore++;
     }
+    roundCount++;
+    console.groupEnd();
 }
 
 
+
 function evalRound(playerPick, cpuPick) {
-    let playerChoice = enumerateRPS(playerPick());
-    let cpuChoice = cpuPick();
-    if (playerChoice === cpuChoice) {
+    if (playerPick === cpuPick) {
         return 'draw';
-    } else if ((playerChoice + 1) % 3 === cpuChoice) {
+    } else if ((playerPick + 1) % 3 === cpuPick) {
         return 'loss';
     } else {
         return 'win';
@@ -34,6 +46,19 @@ function enumerateRPS(string) {
         case 'scissor':
             return 2;
         default:
-            return null;
+            return undefined;
+    }
+}
+
+function stringifyRPS(num) {
+    switch (num){
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+        default:
+            return undefined;
     }
 }
