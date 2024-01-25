fd32b20060a1eafde1341ea673c9706a1d37c76d
Mady-T <adithya5463sajith@gmail.com>
2024-01-26 03:18:28 +0530

    Create game loop (tests pending)

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let playerScore = 0;
 let cpuScore = 0;
 let roundCount = 1;
 
+while (playerScore < 5 && cpuScore < 5) {
+    playRound(playerScore, cpuScore, roundCount);
+}
+
+(playerScore >= 5) ? console.log("You Win!") : console.log("You Lose!");
+
+
 function playRound(playerScore, cpuScore, roundCount) {
     console.group();
     console.log(`Round ${roundCount}:
@@ -11,9 +18,12 @@ function playRound(playerScore, cpuScore, roundCount) {
     
     let playerChoice = prompt("Rock Paper Scissors!").toLowerCase();
     console.log(`You chose ${playerChoice}`);
+
     let cpuPick = Math.floor(Math.random() * 3);
     console.log('CPU chose ${stringifyRPS(cpuPick)}');
+
     let outcome = evalRound(enumerateRPS(playerChoice), cpuPick);
+
     if (outcome === 'win') {
         playerScore++;
     } else if (outcome === 'loss') {
